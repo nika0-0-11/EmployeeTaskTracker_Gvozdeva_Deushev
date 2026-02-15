b791fecee248b792907815b5d7b2ac7ac9785b53
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-02-15 15:36:43 +0300

    Implement customsettersforsalary and experience

--- src/Employee.kt
+++ src/Employee.kt
@@ -1,15 +1,46 @@
-class Employee(
-    fullName: String,
-    position: String,
-    salary: Int,
-    yearsOfExperience: Int
-){
-    var fullName: String = fullName
+class Employee{
+    var fullName: String = ""
         get() = field
 
-    var position: String = position
+    var position: String = ""
         get() = field
 
-    var salary: Int = salary
+    var salary: Int = 0
         get() = field
+        set(value) {
+            if (value < 0) {
+                println("Зарплата не может быть отрицательной")
+            } else {
+                field = value
+            }
+        }
+
+    var yearsOfExperience: Int = 0
+        get() = field
+        set(value) {
+            if (value > 50) {
+                println("\nОпыт работы ограничен 50 годам")
+                field = 50
+            } else
+                field = value
+        }
+}
+
+fun main() {
+    val employee = Employee()
+
+    employee.fullName = "Вероника Гвоздева"
+    employee.position = "Разработчик"
+
+    // Зп отрицательная
+    employee.salary = -1000
+    println("Текущая зп: ${employee.salary}")
+
+    // Зп положительная
+    employee.salary = 50000
+    println("\nТекущая зп: ${employee.salary}")
+
+    // Опыт > 50
+    employee.yearsOfExperience = 60
+    println("Текущий опыт: ${employee.yearsOfExperience}")
 }
