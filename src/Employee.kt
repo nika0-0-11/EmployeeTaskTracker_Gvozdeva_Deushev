080413d7a72ece9ac0dd7dc4214a2fb1f02cab8d
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-02-15 18:02:22 +0300

    Implement assignTaskmethodwith validation

--- src/Employee.kt
+++ src/Employee.kt
@@ -25,8 +25,20 @@ class Employee: ReportGenerator{
                 field = value
         }
 
+    var currentTask: Task? = null
+
     override fun generateReport(): String {
         return "=== Данные сотрудника === \nФИО: $fullName \nДолжность: $position \nЗарплата: $salary руб \nОпыт работы: $yearsOfExperience"
+    }
+
+    fun assignTask(newTask:  Task) {
+        val task = currentTask
+        if (task != null && !task.isCompleted) {
+            println("Сотрудник уже занят задачей: ${task.title}")
+        } else {
+            currentTask = newTask
+            println("Сотруднику назначена задача: ${newTask.title}")
+        }
     }
 }
 
@@ -34,9 +46,11 @@ class Employee: ReportGenerator{
 
 fun main() {
     val employee = Employee()
-
     employee.fullName = "Вероника Гвоздева"
     employee.position = "Разработчик"
+    val task1 = Task("ДЗ - 1", "делать дз", Priority.LOW, false)
+    val task2 = Task("ДЗ - 2", "делать дз", Priority.HIGH, false)
+    val devDepartment = DevelopmentDepartment()
 
     // Зп отрицательная
     employee.salary = -1000
@@ -49,4 +63,11 @@ fun main() {
     // Опыт > 50
     employee.yearsOfExperience = 60
     println("Текущий опыт: ${employee.yearsOfExperience}")
+    println()
+
+    println(devDepartment.generateReport())
+    println()
+    
+    employee.assignTask(task1)
+    employee.assignTask(task2)
 }
